e3bfce3baef8b872450d204ff6ab780c9eab7f29
Hking666 <49830689+Hking666@users.noreply.github.com>
2019-07-18 23:37:59 +0800

    小练习.c

---
+++ Test718.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<windows.h>
+#pragma warning (disable :4996 )
+
+void search(float(*p)[4], int n)
+{
+	int  i, j, flag;
+	for (j = 0; j < n; j++)
+	{
+		flag = 0;
+		for (i = 0; i < 4;i++)
+		if (*(*(p + j) + i) < 60) flag = 1;
+		if (flag == 1)
+		{
+			printf("No.%d fails ,his scores are :\n",j+1 );
+			for (i == 0; i < 4; i++)
+				printf("%5.1f", *(*(p + j) + i));
+			printf("\n");
+		}
+	}
+}
+int main()
+{
+	void search(float(*p)[4], int n);
+	float score[3][4] = { { 65, 57, 70, 60 }, { 58, 87, 90, 81 }, { 90, 99, 100, 98 } };
+	sreach(score, 3);
+	system("pause");
+	return 0;
+}
